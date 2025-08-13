4fda523b25ab2a5aba868ef1f7cb61e06de2e08f
jeffschoe <ratacame@protonmail.com>
2025-08-12 23:42:26 -0400

    CH2L1: TextNode. Created TextNode and TextType classes.

---
+++ src/textnode.py
@@ -0,0 +1,32 @@
+from enum import Enum
+
+
+class TextType(Enum):
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+
+class TextNode:
+    def __init__(self, text, text_type, url=None):
+        self.text = text # the text content of this node
+        self.text_type = text_type # the type of text this node contains, which is a member of the TextType enum.
+        self.url = url # the URL of the link or image
+
+    def __eq__(self, other):
+        # compare all attribues of classes and return TRUE if all are equal
+        return (
+            self.text == other.text
+            and self.text_type == other.text_type
+            and self.url == other.url
+        )
+
+    def __repr__(self):
+        # returns a string presentation of the TextNode object
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})" # .value used to access enum value
+    
+
+
