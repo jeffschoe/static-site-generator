c67871edd182e2fa4bf35139e64a051f154536be
jeffschoe <ratacame@protonmail.com>
2025-08-18 14:14:35 -0400

    CH2L6: TextNode to HTMLNode. Created function to convert text_nodes to HTML LeafNodes. Created unit tests to confirm functionality.

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 
 class TextType(Enum):
@@ -28,5 +29,22 @@ class TextNode: # represents the various types of inline text that can exist in 
         # returns a string presentation of the TextNode object
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})" # .value used to access enum value
     
-
+def text_node_to_html_node(text_node):
+    match text_node.text_type: # match/case statement validates against our TextType class
+        case TextType.TEXT:
+            return LeafNode(None, text_node.text)
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+        case _:  # Default case
+            raise ValueError(f"invalid text type: {text_node.text_type}")
+        
+            
 
