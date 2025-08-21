d1b13022b2b5ec3e98a80dde1d75d855736b4928
jeffschoe <ratacame@protonmail.com>
2025-08-20 23:57:47 -0400

    CH3L4: Extract Links. Added functions to extract images and links using regex.

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -1,28 +1,6 @@
-from textnode import TextType, TextNode
+import re
 
-"""
-def split_nodes_delimiter(old_nodes, delimiter, text_type):
-   
-    new_nodes = []
-    
-    for old_node in old_nodes:
-        if old_node.text_type == TextType.TEXT: # only do parsing on TEXT types
-            sections = old_node.text.split(delimiter)
-            if len(sections) % 2 == 0: # check if it's even, means there are unmatched delimiters
-                raise ValueError(
-                    f"invalid markdown, formatted section not closed, delimiter not closed: {delimiter}"
-                    )
-            # else, delimiters are matched, good
-            for i, section in enumerate(sections):
-                if i % 2 == 0: # index is even, meaning its plain text
-                    new_nodes.append(TextNode(section, TextType.TEXT))
-                else:
-                    new_nodes.append(TextNode(section, text_type))
-        else:
-            new_nodes.append(old_node)
-                
-    return new_nodes 
-"""
+from textnode import TextType, TextNode
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
     new_nodes = []
@@ -43,3 +21,23 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
                 split_nodes.append(TextNode(sections[i], text_type)) # formatted nodes (BOLD, ITALIC, or CODE)
         new_nodes.extend(split_nodes)
     return new_nodes
+
+def extract_markdown_images(text):
+    """
+    Takes raw markdown text and returns a list of tuples. 
+    Each tuple should contain the alt text and the URL of any markdown images
+    return [(alt text, url), (alt text, url)]
+    """
+    pattern = r"!\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
+
+def extract_markdown_links(text):
+    """
+    Extracts markdown links. 
+    It should return tuples of anchor text and URLs
+    return [(anchor text, url), (anchor text, url)]
+    """
+    pattern = r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)"
+    matches = re.findall(pattern, text)
+    return matches
--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -1,6 +1,10 @@
 import unittest
 
-from inline_markdown import split_nodes_delimiter
+from inline_markdown import (
+    split_nodes_delimiter,
+    extract_markdown_images,
+    extract_markdown_links
+    )
 from textnode import TextNode, TextType
 
 
@@ -108,4 +112,42 @@ class TestSplitNodesDelimiter(unittest.TestCase):
                 TextNode("bold", TextType.BOLD),
                 TextNode("alsobold", TextType.BOLD), 
             ]
+        )
+
+class TestImageAndLinkRegex(unittest.TestCase):
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+    
+    def test_extract_markdown_images_multiple(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![image](https://giantwombats.jpg)"
+        )
+        self.assertListEqual(
+            [
+                ("image", "https://i.imgur.com/zjjcJKZ.png"),
+                ("image", "https://giantwombats.jpg")
+            ], 
+            matches
+        )
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev)"
+        )
+        self.assertListEqual([("to boot dev", "https://www.boot.dev")], matches)
+
+    def test_extract_markdown_links_multiple(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) and a link [to good noods](https:www.goodnoodles.yum)"
+        )
+        self.assertListEqual(
+            [
+                ("to boot dev", "https://www.boot.dev"),
+                ("to good noods", "https:www.goodnoodles.yum")
+            ], 
+            matches
         )
