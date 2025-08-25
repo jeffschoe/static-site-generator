1453627cc18f6186e969fd08d3e322eaa18ea694
jeffschoe <ratacame@protonmail.com>
2025-08-24 23:46:41 -0400

    CH4L2: Block Types. Add function to acept a single block and determin the Block Type.

--- src/markdown_blocks.py
+++ src/markdown_blocks.py
@@ -1,3 +1,14 @@
+from enum import Enum
+
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    ULIST = "unordered_list"
+    OLIST = "ordered_list"
+
 
 
 def markdown_to_blocks(markdown):
@@ -9,3 +20,40 @@ def markdown_to_blocks(markdown):
             empty_removed.append(stripped_item)
     return empty_removed
 
+
+def block_to_block_type(block):
+    block_len = len(block)
+    split_lines = block.split("\n")
+    if block.startswith("#"):
+        count = 0
+        # Iterate through the characters of the block
+        for char in block: # count is at the index we are checking each new loop
+            # If the character is a '#', increment the count
+            if char == "#":
+                count += 1
+            # If it's not a '#', we've found the end of the leading hashes, so break the loop
+            else:
+                break
+        if 1 <= count <= 6 and block_len >= count + 2 and block[count] == " ":
+            return BlockType.HEADING
+    if block.startswith("```") and block.endswith("```") and block_len >= 6:
+        return BlockType.CODE
+    if block.startswith(">"):
+        for line in split_lines:
+            if not line.startswith(">"):
+                return BlockType.PARAGRAPH 
+        return BlockType.QUOTE
+    if block.startswith("- "):
+        for line in split_lines:
+            if not line.startswith("- "):
+                return BlockType.PARAGRAPH 
+        return BlockType.ULIST
+    if block[0:3] == "1. ":
+        num = 1
+        for line in split_lines:
+            expected = f"{num}. "
+            if not line.startswith(expected):
+                return BlockType.PARAGRAPH 
+            num += 1
+        return BlockType.OLIST
+    return BlockType.PARAGRAPH 
--- src/test_markdown_blocks.py
+++ src/test_markdown_blocks.py
@@ -2,11 +2,76 @@ import unittest
 
 
 from markdown_blocks import (
-    markdown_to_blocks
+    markdown_to_blocks,
+    block_to_block_type,
+    BlockType,
+
 )
 
 
 class MarkdownToHTML(unittest.TestCase):
+
+    def test_block_to_block_type_paragraph(self):
+        md = "This is **bolded** paragraph"
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_heading_one(self):
+        md = "# Heading one text"
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.HEADING)
+
+    def test_block_to_block_type_heading_two(self):
+        md = "## Heading two text"
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.HEADING)
+
+    def test_block_to_block_type_heading_three(self):
+        md = "### Heading three text"
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.HEADING)
+
+    def test_block_to_block_type_heading_four(self):
+        md = "#### Heading four text"
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.HEADING)
+
+    def test_block_to_block_type_heading_five(self):
+        md = "##### Heading five text"
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.HEADING)
+
+    def test_block_to_block_type_heading_six(self):
+        md = "###### Heading six text"
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.HEADING)
+
+    def test_block_to_block_type_heading_six(self):
+        md = "####### Heading seven text which is an error"
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_code(self):
+        md = "```Code text```"
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.CODE)
+
+    def test_block_to_block_type_quote(self):
+        md = ">Quote text"
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.QUOTE)
+
+    def test_block_to_block_type_unordered_list(self):
+        md = "- Unordered list item 1\n- Unordered list item 2"
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.ULIST)
+
+    def test_block_to_block_type_ordered_list(self):
+        md = "1. Ordered list item 1\n2. Ordered list item 2"
+        result = block_to_block_type(md)
+        self.assertEqual(result, BlockType.OLIST)
+
+
 
     def test_markdown_to_blocks(self):
         md = """
