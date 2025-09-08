f747024ddca085e4e11ab721b4130611f675aa36
jeffschoe <ratacame@protonmail.com>
2025-09-07 22:22:33 -0400

    CH5L1: Copy Static. Created recursive funtions to copy the contents of static/ to public/

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from textnode import TextNode, TextType
+from copy_static import copy_static
 
 def main():
 
@@ -11,7 +12,7 @@ def main():
     
     print(node) # __repr__ method automatically called, no need to explicitly call it
 
-
+    copy_static("static", "public")
 
 if __name__ == "__main__": # evaluates to TRUE if this script/file is run directly, so code below executes. If you import it to someting else, this will be FALSE, so code below will not run automatically
     main() # main function gets executed
