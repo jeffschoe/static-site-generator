62e70696e93ad04fcb26d5c3c879f9be9bf71944
jeffschoe <ratacame@protonmail.com>
2025-09-10 22:46:38 -0400

    CH5L2: Generate Page

--- src/main.py
+++ src/main.py
@@ -2,9 +2,12 @@ import os
 import shutil
 
 from copystatic import copy_files_recursive
+from gencontent import generate_page
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 
 def main():
@@ -15,6 +18,13 @@ def main():
 
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating page...")
+    generate_page(
+        os.path.join(dir_path_content, "index.md"),
+        template_path,
+        os.path.join(dir_path_public, "index.html"),
+    )
 
 if __name__ == "__main__": # evaluates to TRUE if this script/file is run directly, so code below executes. If you import it to someting else, this will be FALSE, so code below will not run automatically
     main() # main function gets executed
